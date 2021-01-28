f63ba93512e13e6f483d77db3983626837b1541b
ryan hill <c16443814@mydit.ie>
2021-01-28 10:13:48 +0000

    Creating Driver table on local postgres database

---
+++ ConnetingToPostgres/src/main/kotlin/ConnetingToPostgres/ConnetingToPostgres/ConnetingToPostgresApplication.kt
@@ -0,0 +1,11 @@
+package ConnetingToPostgres.ConnetingToPostgres
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ConnetingToPostgresApplication
+
+fun main(args: Array<String>) {
+	runApplication<ConnetingToPostgresApplication>(*args)
+}
---
+++ ConnetingToPostgres/src/test/kotlin/ConnetingToPostgres/ConnetingToPostgres/ConnetingToPostgresApplicationTests.kt
@@ -0,0 +1,13 @@
+package ConnetingToPostgres.ConnetingToPostgres
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ConnetingToPostgresApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
